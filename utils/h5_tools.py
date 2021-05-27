74bfe99e82c8ff18edb02203fcccc00b36acf840
Vladimir <syler1983.9@gmail.com>
2021-05-27 21:21:45 +1000

    Add h5 chunked save function and merge_seisan_like_sets script

---
+++ utils/h5_tools.py
@@ -0,0 +1,29 @@
+import h5py as h5
+
+
+def write_batch(path, dataset, batch, string = False):
+    """
+    Writes batch to h5 file
+    :param path: Path to h5 file
+    :param dataset: Name of the dataset
+    :param batch: Data
+    :param string: True - if data should be VL string
+    """
+    with h5.File(path, 'a') as file:
+
+        first = True
+        if dataset in file.keys():
+            first = False
+
+        if not first:
+            file[dataset].resize((file[dataset].shape[0] + batch.shape[0]), axis = 0)
+            file[dataset][-batch.shape[0]:] = batch
+        else:
+            if not string:
+                maxshape = list(batch.shape)
+                maxshape[0] = None
+                maxshape = tuple(maxshape)
+                file.create_dataset(dataset, data = batch, maxshape = maxshape, chunks = True)
+            else:
+                dt = h5.string_dtype(encoding = 'utf-8')
+                file.create_dataset(dataset, data = batch, maxshape = (None,), chunks = True, dtype = dt)
