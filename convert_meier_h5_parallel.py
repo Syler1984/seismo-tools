0440d2cd5c14baa2207408de376e628a823527c2
Vladimir <syler1983.9@gmail.com>
2021-05-30 16:51:11 +1000

    Finish basic parallel meier set conversion script

--- convert_meier_h5_parallel.py
+++ convert_meier_h5_parallel.py
@@ -9,7 +9,7 @@ import multiprocessing as mp
 from utils.h5_tools import write_batch
 
 
-def convert_data(dataset, idx):
+def convert_data(dataset, idx, lock):
     """
     Converts data from meier format to requiered:
         1. Detrend.
@@ -18,7 +18,12 @@ def convert_data(dataset, idx):
         4. Local absolute max normalization.
     """
     # Get data
-    data = dataset[:, idx, :]
+    lock.acquire()
+    try:
+        data = dataset[:, idx, :]
+    finally:
+        lock.release()
+
     channels = [data[i, :] for i in range(data.shape[0])]
 
     d_length = data.shape[1]
@@ -59,8 +64,31 @@ def convert_data(dataset, idx):
     return X.reshape((1, *X.shape))
 
 
-def process(lock, meier_set, span, save_path):
-    print(f'pid {os.getpid()} span: {span[0]}..{span[1]}')
+def process(read_lock, write_lock, path, names_stack, span, save_path, label, id):
+
+    batch_size = span[1] - span[0]
+    X = np.zeros((batch_size, 400, 3))
+    Y = np.full(batch_size, label, dtype = int)
+    Z = np.full(batch_size, id, dtype = object)
+
+    with h5.File(path, 'r') as meier_set:
+
+        for s_name in names_stack:
+            meier_set = meier_set[s_name]
+
+        b = 0
+        for i in range(span[0], span[1]):
+
+            X[b] = convert_data(meier_set, i, read_lock)
+            b += 1
+
+        write_lock.acquire()
+        try:
+            write_batch(save_path, 'X', X)
+            write_batch(save_path, 'Y', Y)
+            write_batch(save_path, 'Z', Z, string = True)
+        finally:
+            write_lock.release()
 
 
 if __name__ == '__main__':
@@ -75,7 +103,7 @@ if __name__ == '__main__':
     parser.add_argument('--procs', help = 'Number of parallel processes, set 0 to run '
                                           'os.cpu_count() number; default: 1', default = 1)
     parser.add_argument('--batch_size', '-b', help = 'Batch size in records (one record is: waveform + label)'
-                                                     ', default: 10000', default = 100)
+                                                     ', default: 10000', default = 1000)
     parser.add_argument('--inspect', help = 'Use this flag to print info about Meier dataset without'
                                             ' performing data conversion', action = 'store_true')
 
@@ -85,45 +113,41 @@ if __name__ == '__main__':
     meier_path = args.meier_path
     save_path = args.save_path
 
-    meier_set_names_stack = ['noise', 'wforms']
+    meier_set_names_stack = ['X']
+    # meier_set_names_stack = ['noise', 'wforms']
 
     batch_size = int(args.batch_size)
 
     label = 2
     _id = 'meier_noise'
 
-    if args.procs:
-        procs = args.procs
+    if int(args.procs):
+        procs = int(args.procs)
     else:
         procs = os.cpu_count()
 
     # Read data
-    meier_set = h5.File(meier_path, 'r')
+    with h5.File(meier_path, 'r') as meier_set:
 
-    for s_name in meier_set_names_stack:
-        meier_set = meier_set[s_name]
+        for s_name in meier_set_names_stack:
+            meier_set = meier_set[s_name]
+
+        meier_inspect_line = str(meier_set)
+        meier_set_length = meier_set.shape[1]
 
     # Print set info?
     if args.inspect:
 
         print('Dataset keys stack: ', meier_set_names_stack)
-        print('Dataset info: ', meier_set)
+        print('Dataset info: ', meier_inspect_line)
         sys.exit(0)
 
     # Convert data
-    b = 0
-
-    X = np.zeros((batch_size, 400, 3))
-    Y = np.full(batch_size, label, dtype = int)
-    Z = np.full(batch_size, _id, dtype = object)
-
     start = int(args.start)
-    end = meier_set.shape[1]
+    end = meier_set_length
 
     if args.end:
         end = int(args.end)
-
-    print(f'Converting data from {start} to {end}..')
 
     data_span = end - start
     last_batch = data_span % batch_size
@@ -131,11 +155,9 @@ if __name__ == '__main__':
     if last_batch:
         batch_num += 1
 
-    import sys
-
+    b = 0
+    print(f'Converting data from {start} to {end}')
     for b in range(batch_num):
-
-        print(f'Converting batch {b} out of {batch_num}..', end = '')
 
         # Get current batch size
         c_batch_size = batch_size
@@ -149,19 +171,24 @@ if __name__ == '__main__':
         c_proc_batch_size = c_batch_size // procs
         c_start_pos = batch_start_pos
         for i in range(procs - 1):
-            c_proc_batch_spans += (c_start_pos, c_start_pos + c_proc_batch_size)
+            c_proc_batch_spans += [(c_start_pos, c_start_pos + c_proc_batch_size)]
             c_start_pos += c_proc_batch_size
 
-        c_proc_batch_spans += (c_start_pos, c_proc_batch_size + c_batch_size % procs)
+        c_proc_batch_spans += [(c_start_pos, c_start_pos + c_proc_batch_size + c_batch_size % procs)]
+
+        print(f'Batch {b} out of {batch_num} '
+              f'(from {c_proc_batch_spans[0][0]} to {c_proc_batch_spans[-1][1]})..', end = '', flush = True)
 
         # Preparing sub-processes
-        lock = mp.Lock()
+        read_lock = mp.Lock()
+        write_lock = mp.Lock()
         processes = []
         for i in range(procs):
-            processes += mp.Process(target = process, args = (lock,
-                                                              meier_set,
-                                                              c_proc_batch_spans[i],
-                                                              save_path))
+            processes += [mp.Process(target = process, args = (read_lock, write_lock,
+                                                               meier_path, meier_set_names_stack,
+                                                               c_proc_batch_spans[i],
+                                                               save_path,
+                                                               label, _id))]
 
         # Process batch
         for i in range(procs):
@@ -172,33 +199,3 @@ if __name__ == '__main__':
             processes[i].join()
 
         print('\t\t..saved!')
-
-    sys.exit(0)
-
-    for i in range(start, end):
-
-        if not i % 100:
-            print('idx: ', i)
-
-        X[b] = convert_data(meier_set, i)
-
-        b += 1
-
-        if b == batch_size:
-
-            b = 0
-
-            write_batch(save_path, 'X', X)
-            write_batch(save_path, 'Y', Y)
-            write_batch(save_path, 'Z', Z, string = True)
-
-            X = np.zeros((batch_size, 400, 3))
-
-            print('..batch saved!')
-            print('-' * 25)
-
-    if b:
-
-        write_batch(save_path, 'X', X[:b])
-        write_batch(save_path, 'Y', Y[:b])
-        write_batch(save_path, 'Z', Z[:b], string = True)
