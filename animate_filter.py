b892d4333c58534573da1f942306e6e35d05659b
Vladimir <syler1983.9@gmail.com>
2021-09-13 12:27:24 +1000

    Update animate_filter.py

--- animate_filter.py
+++ animate_filter.py
@@ -26,13 +26,17 @@ pause = False
 save_fps = 60
 animation_dt = 10
 
-stream_path = 'C:/data/seismic_streams/TMSK.D0.00.SHZ.2015.347'
-save_name = 'filter.gif'
+stream_path = 'C:/data/seismic_streams/NYSH.IM.00.EHZ.2021.091'
+save_animation = True
+save_name = 'filter_60sec.gif'
 
-slice_start = 120
-slice_end = 180
+slice_start = '2021-04-01T12:35:30'
+slice_end = 60
 
-highpass_frequencies = [0.0, (0.000001, 0.001, 100)]
+highpass_frequencies = [(0., 0., 20), (0.0001, 0.001, 100),
+                        (0.001, 0.1, 300), (0.1, 0.5, 100), (0.5, 1., 50), (1., 2., 100), (2., 2., 100)]
+
+normalize_stream = True
 
 
 # Functions
@@ -46,7 +50,7 @@ def prepare_plot():
     return figure, axes, plot
 
 
-def plot_filtered(figure, axes, plot, stream, freq, do_filter):
+def plot_filtered(figure, axes, plot, stream, freq, frame, do_filter):
 
     if len(stream) > 1:
         raise AttributeError('Stream has more than one trace on sliced data! '
@@ -57,16 +61,46 @@ def plot_filtered(figure, axes, plot, stream, freq, do_filter):
 
     if do_filter:
         stream.filter('highpass', freq=freq)
-        figure.suptitle(f'highpass = {freq}')
+        if freq >= 0.:
+            figure.suptitle(f'highpass = {freq:.2f} Hz')
+        elif freq >= 0.1:
+            figure.suptitle(f'highpass = {freq:.4f} Hz')
+        else:
+            figure.suptitle(f'highpass = {freq:.5e}')
     else:
-        figure.suptitle('no filter')
+        figure.suptitle(f'no filter')
 
     axes.set_xlim(0, stream[0].data.shape[0])
     axes.set_ylim(np.min(stream[0].data), np.max(stream[0].data))
     plot.set_data(np.array(range(stream[0].data.shape[0])), stream[0].data)
 
 
-def plot_stream(frequency, figure, axes, plot, stream, start=None, end=None):
+def plot_stream(data, figure, axes, plot, stream):
+    frame, frequency = data
+
+    if frequency == 0.:
+        plot_filtered(figure, axes, plot, stream, frequency, frame, False)
+    else:
+        plot_filtered(figure, axes, plot, stream, frequency, frame, True)
+
+    return plot
+
+
+def frequencies() -> object:
+    global highpass_frequencies
+    i = 0
+    for x in highpass_frequencies:
+        if type(x) is tuple:
+            f_range = np.linspace(x[0], x[1], x[2])
+            for y in f_range:
+                yield i, y
+                i += 1
+        else:
+            yield i, x
+            i += 1
+
+
+def prepare_stream(stream, start=None, end=None, normalize=False):
     if not start:
         start = stream[0].stats.starttime
     elif type(start) in [float, int]:
@@ -76,29 +110,14 @@ def plot_stream(frequency, figure, axes, plot, stream, start=None, end=None):
     if not end:
         end = stream[-1].stats.endtime
     elif type(end) in [float, int]:
-        end = start + end*60.
+        end = start + end
     else:
         end = UTCDateTime(end)
 
     stream = stream.slice(start, end)
-
-    if frequency == 0.:
-        plot_filtered(figure, axes, plot, stream, frequency, False)
-    else:
-        plot_filtered(figure, axes, plot, stream, frequency, True)
-
-    return plot
-
-
-def frequencies() -> object:
-    global highpass_frequencies
-    for x in highpass_frequencies:
-        if type(x) is tuple:
-            f_range = np.linspace(x[0], x[1], x[2])
-            for y in f_range:
-                yield y
-        else:
-            yield x
+    if normalize:
+        stream.normalize()
+    return stream
 
 
 if __name__ == '__main__':
@@ -109,15 +128,16 @@ if __name__ == '__main__':
     s_end = st[-1].stats.endtime.strftime("%d.%m.%YT%H:%M:%S")
     print(f'Read seismic stream with {len(st)} traces and time span: {s_start} - {s_end}')
 
-    max_time = 10
-    plot_dt = 1
-
+    st = prepare_stream(st, slice_start, slice_end, normalize_stream)
     figure, axes, plot = prepare_plot()
 
     animation = FuncAnimation(figure, plot_stream, frequencies,
-                              fargs=[figure, axes, plot, st, slice_start, slice_end],
-                              blit=False, interval=animation_dt, repeat=True)
+                              fargs=[figure, axes, plot, st],
+                              blit=False, interval=animation_dt, repeat=True, save_count=600)
 
     # Display the animation
     plt.show()
-    # animation.save(save_name, fps=save_fps)
+
+    if save_animation:
+        print(f'Saving animation to a file: "{save_name}"')
+        animation.save(save_name, fps=save_fps)
