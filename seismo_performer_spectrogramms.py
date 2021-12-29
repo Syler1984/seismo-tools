f6cbe9ef6ae1232f1ac7bb6090b18af8e588ff33
Vladimir <syler1983.9@gmail.com>
2021-12-30 02:35:44 +1000

    Update seismo_performer_spectrogramms.py

--- seismo_performer_spectrogramms.py
+++ seismo_performer_spectrogramms.py
@@ -1,16 +1,24 @@
+import argparse
 import utils.fast_attention as models
 
 
 if __name__ == '__main__':
 
-    cnn = False
+    print('Parsing arguments..')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cnn', help='Use cnn model', action='store_true')
+    args = parser.parse_args()
 
     weights = {
-        'favor': 'utils/fast_attention/weights/w_model_cnn_spec.hd5',
-        'cnn': 'utils/fast_attention/weights/w_model_performer_with_spec.hd5',
+        'favor': 'utils/fast_attention/weights/w_model_performer_with_spec.hd5',
+        'cnn': 'utils/fast_attention/weights/w_model_cnn_spec.hd5',
     }
 
-    if cnn:
+    if args.cnn:
+        print('Loading CNN model..')
         model = models.load_cnn(weights['cnn'])
     else:
+        print('Loading Seismo-Performer..')
         model = models.load_performer(weights['favor'])
+
+    model.summary()
